d8baac2d8b5091f4dbe2e0dc0e39895c8703033b
sonvofriend <sonvofriend@gmail.com>
2019-05-30 10:24:56 +0700

    update

--- app/src/main/java/dhbkdn/it/dut/smartbot/MainActivity.java
+++ app/src/main/java/dhbkdn/it/dut/smartbot/MainActivity.java
@@ -49,31 +49,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            mAuth.signOut();
-            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private void addListener() {
         lvListEquipment.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
@@ -88,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     }
 
-    ;
+
 
     private void inits() {
         mAuth = FirebaseAuth.getInstance();
@@ -154,6 +129,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
 
+    }
+
+    public void Logout(View view) {
+        mAuth.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
 
